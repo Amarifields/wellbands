a0ff615573dc381bf0fff52f1ea01d6a91ed6134
Amarifields <sumbusinessinquires@gmail.com>
2024-03-07 23:43:22 -0500

    few changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,14 +12,17 @@ const Navbar = () => {
     { name: "Community", path: "/community" },
   ];
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="bg-transparent w-full ">
+    <div className="bg-transparent w-full">
       <div className="container mx-auto px-6 py-6 lg:py-6">
         <nav className="flex justify-between items-center">
           <Link to="/">
             <img src={logo} alt="Wellbands Logo" className="h-10 lg:h-12" />
           </Link>
-
           <ul className="hidden lg:flex gap-8 items-center">
             {links.map((link, index) => (
               <li
@@ -30,7 +33,7 @@ const Navbar = () => {
               </li>
             ))}
             <li className="text-gray-600 font-medium hover:text-blue-500 transition-colors duration-200 relative">
-              <Link className="group" disabled>
+              <Link to="#" className="group" disabled>
                 Shop
                 <div className="absolute invisible group-hover:visible mt-5 w-auto p-2 bg-gray-900 text-white rounded-lg shadow-lg">
                   Coming Soon
@@ -50,21 +53,21 @@ const Navbar = () => {
           </div>
 
           {isOpen && (
-            <ul className="flex flex-col items-center absolute bg-gray-100 w-full md:w-[720px] top-20  mt-10 rounded-lg  z-20 lg:hidden">
+            <ul className="flex flex-col items-center fixed inset-x-0 top-20 pt-5 bg-gray-100 rounded-b-lg shadow-lg transition-transform duration-300 ease-in-out lg:hidden">
               {links.map((link, index) => (
                 <li
                   key={index}
-                  className="text-gray-600 hover:text-blue-500 w-full p-2 text-center border-b transition-colors duration-200"
+                  className="text-gray-600 hover:text-blue-500 w-full p-3 text-center border-b transition-colors duration-200"
                 >
-                  <Link to={link.path} onClick={() => setIsOpen(false)}>
+                  <Link to={link.path} onClick={handleLinkClick}>
                     {link.name}
                   </Link>
                 </li>
               ))}
-              <li className="text-gray-600 hover:text-blue-500 w-full p-2 text-center transition-colors duration-200">
-                <Link to="/shop" onClick={() => setIsOpen(false)}>
+              <li className="text-gray-600 hover:text-blue-500 w-full p-3 text-center">
+                <span onClick={handleLinkClick} className="cursor-not-allowed">
                   Shop
-                </Link>
+                </span>
               </li>
             </ul>
           )}
