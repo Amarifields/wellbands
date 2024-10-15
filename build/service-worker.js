b0454bcc19cff979e081b5d5f187bae4d81023a7
Amarifields <sumbusinessinquires@gmail.com>
2024-10-14 23:50:12 -0400

    Add build folder for deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ae8d96a3ee0e010d7e07852cc7bddf8.js"
+  "/precache-manifest.b80b16ff6261c2ff869771d728fb425e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
